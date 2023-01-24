f90c649887df187bfe32bd6333d0901b30b0f6bf
Danil Savr <danukan74@gmail.com>
2023-01-24 22:15:37 +0500

    Homework_2

--- Homework_1/Peasant.java
+++ Homework_1/Peasant.java
@@ -1,9 +1,10 @@
+import java.util.ArrayList;
 public class Peasant extends BaseHero
 {
     boolean supply;
 
-    public Peasant(String name, String role, int attack, int defence, int[] damage, int health, int speed,boolean supply) {
-        super(name, role, attack, defence, damage, health, speed);
+    public Peasant(String name, String role, int attack, int defence, int[] damage, int maxHealth, int speed,boolean supply) {
+        super(name, role, attack, defence, damage, maxHealth, speed);
         this.supply = supply;
     }
     public Peasant(String name){
@@ -14,5 +15,9 @@ public class Peasant extends BaseHero
     @Override
     public String toString() {
         return super.toString() + ", Supply: "+ supply;
+    }
+    @Override
+    public void step(ArrayList<BaseHero> heroList) {
+
     }
 }
--- Homework_1/Ranged.java
+++ Homework_1/Ranged.java
@@ -1,16 +1,21 @@
+import java.util.ArrayList;
 public class Ranged extends BaseHero{
 
     int shoots;
     
 
-    public Ranged(String name, String role, int attack, int defence, int[] damage, int health, int speed,int shoots) 
+    public Ranged(String name, String role, int attack, int defence, int[] damage, int maxHealth, int speed,int shoots) 
     {
-        super(name, role, attack, defence, damage, health, speed);
+        super(name, role, attack, defence, damage, maxHealth, speed);
         this.shoots=shoots;
     }
     
     @Override
     public String toString() {
         return super.toString() + ", Shoots: " + shoots;
+    }
+    @Override
+    public void step(ArrayList<BaseHero> heroList) {
+
     }
 }
